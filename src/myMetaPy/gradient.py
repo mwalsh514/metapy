ba0ff5f5edbaf086a0fe869bc10f3a0395e3d1c2
mwalsh514 <113800020+mwalsh514@users.noreply.github.com>
2025-10-20 20:27:20 -0400

    Add gradient algorithm stubs and requirements file
    Created requirements.txt for dependency management. Added initial stubs and comments for three gradient-based optimization algorithms in gradient.py to outline future implementation.

---
+++ src/myMetaPy/gradient.py
@@ -0,0 +1,14 @@
+# Algorithm 1: Gradient Ascent
+# def = gradientAscent()
+
+
+
+
+
+
+
+# Algorithm 2: Newton's Method (Adapted for Optima Finding)
+
+
+
+# Algorithm 3: Gradient Ascent with Restarts
